65718f7b4e9f2227a0740df3478faff02dd88f9b
zhenxing133 <zhenxing133@163.com>
2018-03-06 14:24:11 +0800

    Initial commit

---
+++ app/src/main/java/lytech/com/demos/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package lytech.com.demos.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import lytech.com.demos.R;
+
+/**
+ * Created by yuan.zhen.xing on 2018-03-06.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
